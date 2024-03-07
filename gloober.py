08ff27274196affa319eae8909c60d2c17ec85df
J. D. Moss <jusdmoss760@gmail.com>
2024-03-07 13:20:44 -0500

    First commit, main file only

---
+++ gloober.py
@@ -0,0 +1,15 @@
+from nerd import Talker
+def main ():
+    # This comment is an added line in the file.
+    print ("This has been modified from the original code.")
+    t = Talker ()
+    t. speak ("Hi")
+    t. speak ("Hello way over there!", True)
+    
+if __name__ == " main":
+    main()
+
+a = "hello"
+b = " nerd"
+#this is a comment!!
+print (a,b)
